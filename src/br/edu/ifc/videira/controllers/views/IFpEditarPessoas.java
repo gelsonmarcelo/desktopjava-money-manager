77182aee6f2dc10790d3c68a0e558d446a465a68
Gelson Nascimento <gelsonschikorski@hotmail.com>
2021-04-11 08:59:03 -0300

    Finalização das validações na tela de edição de pessoas.

--- src/br/edu/ifc/videira/controllers/views/IFpEditarPessoas.java
+++ src/br/edu/ifc/videira/controllers/views/IFpEditarPessoas.java
@@ -34,17 +34,16 @@ public class IFpEditarPessoas extends JInternalFrame {
 	UsuarioDao usDao = new UsuarioDao();
 	private JTextField tfCod;
 	private JNumberFormatField tfSaldo;
-	
+
 	Quem qm = new Quem();
 	QuemDao qd = new QuemDao();
 	private JTextField tfContato;
 	private JCheckBox cboxSaldoNegativo;
 	private JButton btRegistrar;
 	private JButton btAtualizar;
-	
+
 	public IFpEditarPessoas() {
-		super("Editar Pessoas # " + (++IFuLogin.openFrameCount) + "º", 
-				false, // resizable
+		super("Editar Pessoas # " + (++IFuLogin.openFrameCount) + "º", false, // resizable
 				true, // closable
 				false, // maximizable
 				true);// iconifiable
@@ -74,7 +73,7 @@ public class IFpEditarPessoas extends JInternalFrame {
 		tfNome.setBounds(124, 301, 410, 34);
 		getContentPane().add(tfNome);
 		tfNome.setColumns(10);
-		
+
 		JScrollPane spPessoas = new JScrollPane();
 		spPessoas.setBounds(23, 66, 511, 223);
 		getContentPane().add(spPessoas);
@@ -86,37 +85,39 @@ public class IFpEditarPessoas extends JInternalFrame {
 				if (table.getSelectedRow() != -1) {
 					tfCod.setText(String.valueOf(table.getValueAt(table.getSelectedRow(), 0)));
 					tfNome.setText(String.valueOf(table.getValueAt(table.getSelectedRow(), 1)));
-					tfSaldo.setText(String.valueOf(table.getValueAt(table.getSelectedRow(), 2)));
+					String saldo = String.valueOf(table.getValueAt(table.getSelectedRow(), 2));
+					tfSaldo.setText(saldo);
+					if (Double.parseDouble(saldo) < 0) {
+						cboxSaldoNegativo.setSelected(true);
+					}else {
+						cboxSaldoNegativo.setSelected(false);
+					}
 					btRegistrar.setEnabled(false);
+					btRegistrar.setToolTipText("Clique no botão de limpar para habilitar um novo registro");
 					btAtualizar.setEnabled(true);
-					
+					btAtualizar.setToolTipText("Atualizar a pessoa modificada");
 				}
 			}
 		});
 		table.setFont(MainInternalFrame.fonteTabela);
 		spPessoas.setViewportView(table);
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"C\u00F3digo", "Nome", "Saldo", "Contato"
-			}
-		));
+		table.setModel(
+				new DefaultTableModel(new Object[][] {}, new String[] { "C\u00F3digo", "Nome", "Saldo", "Contato" }));
 		table.getColumnModel().getColumn(1).setPreferredWidth(383);
 		atualizarTabela();
-		
+
 		btRegistrar = new JButton("Registrar");
 		btRegistrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
-				if(!tfNome.getText().isEmpty()) {
+
+				if (!tfNome.getText().isEmpty()) {
 					qm.setNome(tfNome.getText());
-					if(cboxSaldoNegativo.isSelected()) {
+					if (cboxSaldoNegativo.isSelected()) {
 						qm.setSaldo(tfSaldo.getText(), true);
-					}else {
+					} else {
 						qm.setSaldo(tfSaldo.getText(), false);
 					}
-					
+
 					try {
 						qd.cadastrarPessoa(qm);
 						limpar();
@@ -128,74 +129,84 @@ public class IFpEditarPessoas extends JInternalFrame {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-				}else {
-					JOptionPane.showMessageDialog(null, "Pelo menos o nome da pessoa deve ser preenchido para cadastrar.", "Campo obrigatório em branco", JOptionPane.WARNING_MESSAGE);
+				} else {
+					JOptionPane.showMessageDialog(null,
+							"Pelo menos o nome da pessoa deve ser preenchido para cadastrar.",
+							"Campo obrigatório em branco", JOptionPane.WARNING_MESSAGE);
 				}
 			}
 		});
-		btRegistrar.setToolTipText("Necessita autentica\u00E7\u00E3o");
+		btRegistrar.setToolTipText("Salvar um novo registro");
 		btRegistrar.setFont(MainInternalFrame.fonte4);
 		btRegistrar.setBounds(23, 448, 144, 32);
 		getContentPane().add(btRegistrar);
-		
+
 		btAtualizar = new JButton("Atualizar");
 		btAtualizar.setEnabled(false);
+		btAtualizar.setToolTipText("Selecione uma pessoa na tabela para habilitar a atualização");
 		btAtualizar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				qm.setCodigo(Integer.parseInt(tfCod.getText()));
-				qm.setNome(tfNome.getText());
-				qm.setSaldo(tfSaldo.getText(), cboxSaldoNegativo.isSelected());
-				
-				try {
-					qd.atualizarPessoa(qm);
-					atualizarTabela();
-					limpar();
-					
-				} catch (Exception e1) {
-					e1.printStackTrace();
+
+				if (!tfNome.getText().isEmpty()) {
+					qm.setCodigo(Integer.parseInt(tfCod.getText()));
+					qm.setNome(tfNome.getText());
+					qm.setSaldo(tfSaldo.getText(), cboxSaldoNegativo.isSelected());
+					try {
+						qd.atualizarPessoa(qm);
+						atualizarTabela();
+						limpar();
+
+					} catch (Exception e1) {
+						e1.printStackTrace();
+					}
+				} else {
+					JOptionPane.showMessageDialog(null,
+							"Pelo menos o nome da pessoa deve ser preenchido para atualizar",
+							"Campo obrigatório em branco", JOptionPane.WARNING_MESSAGE);
 				}
 			}
 		});
-		btAtualizar.setToolTipText("Necessita autentica\u00E7\u00E3o");
+		// ### - btAtualizar.setToolTipText("Necessita autentica\u00E7\u00E3o");
 		btAtualizar.setFont(MainInternalFrame.fonte4);
 		btAtualizar.setBounds(209, 448, 144, 32);
 		getContentPane().add(btAtualizar);
-		
+
 		tfCod = new JTextField();
 		tfCod.setFont(MainInternalFrame.fonte4);
 		tfCod.setColumns(10);
 		tfCod.setBounds(0, 483, 31, 28);
+		tfCod.setVisible(false);
 		getContentPane().add(tfCod);
-		
+
 		tfSaldo = new JNumberFormatField();
 		tfSaldo.setFont(new Font("Calibri", Font.PLAIN, 20));
 		tfSaldo.setColumns(10);
 		tfSaldo.setBounds(124, 390, 149, 34);
 		getContentPane().add(tfSaldo);
-		
+
 		JLabel lbSaldo = new JLabel("Saldo:");
 		lbSaldo.setFont(MainInternalFrame.fonte4);
 		lbSaldo.setBounds(23, 390, 102, 34);
 		getContentPane().add(lbSaldo);
-		
+
 		JButton btLimpar = new JButton("Limpar");
 		btLimpar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				limpar();
 			}
 		});
-		btLimpar.setToolTipText("Necessita autentica\u00E7\u00E3o");
+		btLimpar.setToolTipText("Limpar seleção/valores inseridos");
 		btLimpar.setFont(MainInternalFrame.fonte4);
 		btLimpar.setBounds(390, 448, 144, 32);
 		getContentPane().add(btLimpar);
-		
+
 		JLabel lbContato = new JLabel("Contato:");
-		lbContato.setToolTipText("N\u00E3o dospon\u00EDvel ainda");
+		lbContato.setToolTipText("N\u00E3o dispon\u00EDvel ainda");
 		lbContato.setEnabled(false);
 		lbContato.setFont(new Font("Sitka Subheading", Font.PLAIN, 25));
 		lbContato.setBounds(23, 345, 102, 34);
 		getContentPane().add(lbContato);
-		
+
 		tfContato = new JTextField();
 		tfContato.setToolTipText("N\u00E3o dispon\u00EDvel ainda");
 		tfContato.setEnabled(false);
@@ -203,15 +214,16 @@ public class IFpEditarPessoas extends JInternalFrame {
 		tfContato.setColumns(10);
 		tfContato.setBounds(124, 345, 410, 34);
 		getContentPane().add(tfContato);
-		
+
 		cboxSaldoNegativo = new JCheckBox("Saldo negativo");
 		cboxSaldoNegativo.setFont(MainInternalFrame.fonte5);
 		cboxSaldoNegativo.setBounds(279, 397, 255, 23);
 		getContentPane().add(cboxSaldoNegativo);
-		
+
 		atualizarTabela();
 
 	}
+
 	/**
 	 * Limpa campos
 	 */
@@ -221,9 +233,13 @@ public class IFpEditarPessoas extends JInternalFrame {
 		tfSaldo.setText("");
 		cboxSaldoNegativo.setSelected(false);
 		btAtualizar.setEnabled(false);
+		btAtualizar.setToolTipText("Selecione uma pessoa na tabela para habilitar a atualização");
 		btRegistrar.setEnabled(true);
+		btRegistrar.setToolTipText("Salvar um novo registro");
+		table.clearSelection();
+
 	}
-	
+
 	/**
 	 * Pega informações do banco e atualiza a tabela
 	 */
